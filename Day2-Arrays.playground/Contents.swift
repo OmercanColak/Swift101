0d66db01c7ac32717526e2d1642a350579b79982
OmercanColak <omercancolak06@gmail.com>
2023-03-14 16:20:48 +0300

    Day2 | Variables

---
+++ Day2-Arrays.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var myFavoriteMovies = ["Pulp Fiction","Kill Bill","Reservoir Dogs",5,true] as [Any]
+
+// as -> casting
+// Any -> any object can be add on arrays
+
+//index
+myFavoriteMovies[0]
+myFavoriteMovies[1]
+myFavoriteMovies[2]
+myFavoriteMovies[3]
+myFavoriteMovies[4]
+
+
+var myStringArray = ["Test5","Test2","Test1","Test4"]
+
+myStringArray[0].uppercased()
+
+//içinde kaç tane elaman var
+myStringArray.count
+
+//dizideki son eleman
+myStringArray[myStringArray.count - 1]
+myStringArray.last
+
+//Alfabatik sıralıyor
+myStringArray.sort()
+
+
+var myNumberArray = [1,2,3,4,5,6,7]
+
+
