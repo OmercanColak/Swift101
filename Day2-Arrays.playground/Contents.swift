2250d2e92a9f413ed244c5da8bf8650087e2b93f
OmercanColak <omercancolak06@gmail.com>
2023-03-14 16:45:59 +0300

    Day2 | Variables

--- Day2-Arrays.playground/Contents.swift
+++ Day2-Arrays.playground/Contents.swift
@@ -28,6 +28,24 @@ myStringArray.last
 myStringArray.sort()
 
 
-var myNumberArray = [1,2,3,4,5,6,7]
+var myNumberArray = [1,2,3,4,5,6,7,1,3,5,6]
+myNumberArray.append(8) //sonuna eleman ekler
+myNumberArray.last
 
+//Set (Unordered Collection(Sırasız Koleksiyon))
+//Setler dizi gibi fakat indexleme yok. Aynı elemandan sadece 1 tane olabilir.
 
+var mySet : Set = [1,2,3,4,5,3,4,6]
+var myStringSet : Set = ["a","b","c","a"]
+
+var myInternetArray = [1,2,3,1,2,5,6,2,1]
+var myInternetSet = Set(myInternetArray)
+print(myInternetArray) //diziyi birebir gösterir.
+print(myInternetSet) //Tekrarlanan değerler görünmez ve karışık görünür.
+
+var mySet1 : Set = [1,2,3]
+var mySet2 : Set = [3,4,5]
+
+var mySet3 = mySet1.union(mySet2) //Union birleştirmeye yarar.
+
+print(mySet3)
