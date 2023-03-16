9a8f2efcfd5e3e4fd92a4e12d50df53c994a0b78
OmercanColak <omercancolak06@gmail.com>
2023-03-16 15:45:24 +0300

    Day4 || If Control Added

---
+++ Day4-IfControl.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var myAge = 25
+
+//<, >, ==, !=
+//AND &&
+// OR ||
+ 
+if myAge < 20 {
+    print("20 -")
+}else if myAge > 20 && myAge < 30 {
+    print("20s")
+}else if myAge > 30 && myAge < 40 {
+    print("30s")
+}else {
+    print("40s")
+}
+
+var myString = "James"
+
+if myString == "James" {
+    print("Yes")
+}
